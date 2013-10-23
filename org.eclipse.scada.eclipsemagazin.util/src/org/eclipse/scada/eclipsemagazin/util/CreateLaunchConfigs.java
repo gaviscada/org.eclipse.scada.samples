42b0fa09c27c99428ec7b5d55ad52218fe3d5109
Juergen Rose <juergen.rose@ibh-systems.com>
2013-10-23 09:34:49 +0200

    initial checkin master config

---
+++ org.eclipse.scada.eclipsemagazin.util/src/org/eclipse/scada/eclipsemagazin/util/CreateLaunchConfigs.java
@@ -0,0 +1,12 @@
+package org.eclipse.scada.eclipsemagazin.util;
+
+public class CreateLaunchConfigs {
+	public static void main(String[] args) {
+		String path = "../org.eclipse.scada.eclipsemagazin.master";
+		new CreateLaunchConfigs().run();
+	}
+
+	private void run() {
+		
+	}
+}
