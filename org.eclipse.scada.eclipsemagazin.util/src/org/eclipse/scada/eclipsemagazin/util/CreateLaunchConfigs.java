00c142abaa585333fe0ea91da509715163f1ec45
Juergen Rose <juergen.rose@ibh-systems.com>
2013-10-23 10:03:56 +0200

    convert to plugin project to easily handle dependencies

--- org.eclipse.scada.eclipsemagazin.util/src/org/eclipse/scada/eclipsemagazin/util/CreateLaunchConfigs.java
+++ org.eclipse.scada.eclipsemagazin.util/src/org/eclipse/scada/eclipsemagazin/util/CreateLaunchConfigs.java
@@ -1,12 +1,20 @@
 package org.eclipse.scada.eclipsemagazin.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.io.DirectoryWalker;
+
 public class CreateLaunchConfigs {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		String path = "../org.eclipse.scada.eclipsemagazin.master";
-		new CreateLaunchConfigs().run();
+		new CreateLaunchConfigs().run(path);
 	}
 
-	private void run() {
-		
+	private void run(String path) throws IOException {
+		List<File> profiles = new ProfileWalker().gather(new File(path));
+		System.out.println(profiles);
 	}
 }
---
+++ org.eclipse.scada.eclipsemagazin.util/src/org/eclipse/scada/eclipsemagazin/util/ProfileWalker.java
@@ -0,0 +1,26 @@
+package org.eclipse.scada.eclipsemagazin.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.io.DirectoryWalker;
+
+public class ProfileWalker extends DirectoryWalker<File> {
+	
+	public List<File> gather(File startDirectory) throws IOException {
+		List<File> results = new ArrayList<File>();
+		walk(startDirectory, results);
+		return results;
+	}
+
+	@Override
+	protected void handleFile(File file, int depth, Collection<File> results)
+			throws IOException {
+		if (file.getName().endsWith(".profile")) {
+			results.add(file);
+		}
+	}
+}
